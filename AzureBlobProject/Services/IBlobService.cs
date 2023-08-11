5ce7dedb0fea62253c02abd6ce0bd395107875f0
livingstone23 <livingstone23@gmail.com>
2023-08-11 07:06:40 +0200

    Initial charge

---
+++ AzureBlobProject/Services/IBlobService.cs
@@ -0,0 +1,21 @@
+﻿using AzureBlobProject.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureBlobProject.Services;
+
+public interface IBlobService
+{
+
+
+    Task<string> GetBlob(string name, string containerName);
+    Task<List<string>> GetAllBlobs(string containerName);
+    Task<bool> UploadBlob(string name, IFormFile file, string containerName, Blob blob);
+    Task<bool> DeleteBlob(string name, string containerName);
+
+    //The last created
+    Task<List<Blob>> GetAllBlobsWithUri(string containerName);
+
+
+}
